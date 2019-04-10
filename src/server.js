dfa67a13056c04dd4b5b94aaaa7fbc6a41141c13
kevin <kevin@cpqd.com.br>
2019-04-10 16:27:30 -0300

    Implementa backend da aplicação

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const cors = require('cors')
+
+app.use(cors())
+
+io.on('connection', socket => {
+   socket.on('connectRoom', box => {
+       socket.join(box)
+   })
+})
+
+mongoose.connect('mongodb+srv://kevin:uehara@cluster0-uczgu.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true
+})
+
+app.use((req, res, next) => {
+    req.io = io
+    return next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+app.use(require('./routes'))
+
+
+server.listen(process.env.PORT || 3000)
